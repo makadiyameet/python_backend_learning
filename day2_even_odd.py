3a4c4ec3c7ee83f93120895331824971f466d590
makadiyameet <makadiyameet1@gmail.com>
2026-03-05 13:45:55 +0100

    day2_progress

---
+++ day2_even_odd.py
@@ -0,0 +1,20 @@
+# Today’s focus is decision making in Python, which is essential for backend logic.
+
+# 📚 Concepts to Study
+
+# if statements
+
+# elif
+
+# else
+
+# Comparison operators: ==, !=, >, <, >=, <=
+
+# Modulo operator %
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
