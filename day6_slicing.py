4a08ea5d9260f0c1cd645f5c291e58353e28d2ba
makadiyameet <makadiyameet1@gmail.com>
2026-03-11 15:56:06 +0100

    day6_progress

---
+++ day6_slicing.py
@@ -0,0 +1,16 @@
+# Day 6 – List Slicing & Basic Data Processing
+# Concepts Today
+
+# List slicing
+
+# len() function
+
+# Finding max value
+
+# Basic data processing (backend style)
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+print(numbers[0:3])
+print(numbers[2:5])
+print(numbers[3:])
